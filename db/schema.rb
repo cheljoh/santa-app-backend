89718ac087bc087a6ef2e42e71dc41328920c7f4
Chelsea Johnson <chelseajohnson@macbook-air.lan>
2023-12-19 13:47:45 -0700

    enable pgcrypto for uuids

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_26_185209) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_19_202839) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
